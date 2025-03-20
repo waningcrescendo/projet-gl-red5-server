4093d45fef75ec0e8ce074aa466e55bca50c1d7c
Parreno Raphaela <raphaela.parreno.etu@a13p8.fil.univ-lille.fr>
2025-03-20 15:40:03 +0100

    fix(test): Modification de 2 tests pour qu'ils passent

--- io/src/test/java/org/red5/io/AMF3IOTest.java
+++ io/src/test/java/org/red5/io/AMF3IOTest.java
@@ -133,7 +133,8 @@ public class AMF3IOTest extends AbstractIOTest {
     log.debug("Dataframe params type: {}", objectType);
     Map<Object, Object> params;
     if (objectType == DataTypes.CORE_MAP) {
-      // the params are sent as a Mixed-Array. Required to support the RTMP publish provided by
+      // the params are sent as a Mixed-Array. Required to support the RTMP publish
+      // provided by
       // ffmpeg
       params = (Map<Object, Object>) in.readMap();
     } else if (objectType == DataTypes.CORE_ARRAY) {
@@ -150,26 +151,26 @@ public class AMF3IOTest extends AbstractIOTest {
     // reset for next test
     resetOutput();
     ///////////////////////////////////////////////
-    //        log.debug("\n onCuePoint");
-    //        // OSMF CuePoint object
-    //        buf.put(IOUtils.hexStringToByteArray("0002000a6f6e437565506f696e74110a03"));
-    //        // AS3 Object with cuepoint properties
+    // log.debug("\n onCuePoint");
+    // // OSMF CuePoint object
+    // buf.put(IOUtils.hexStringToByteArray("0002000a6f6e437565506f696e74110a03"));
+    // // AS3 Object with cuepoint properties
     //
     // buf.put(IOUtils.hexStringToByteArray("0002000a6f6e437565506f696e74110a0b01096e616d6506176e6176437565506f696e740974797065060b6576656e740974696d65040115706172616d65746572730a01176f7269656e746174696f6e045a01"));
-    //        buf.flip();
-    //        // skip the first byte
-    //        assertEquals(0, buf.get());
-    //        // validate data
-    //        assertEquals(DataTypes.CORE_STRING, in.readDataType());
-    //        str = in.readString();
-    //        assertEquals("onCuePoint", str);
-    //        //Object object = Deserializer.deserialize(in, String.class);
-    //        log.debug("onCuePoint: {}", str);
-    //        //assertEquals(DataTypes.CORE_OBJECT, in.readDataType());
-    //        object = Deserializer.deserialize(in, MetaCue.class);
-    //        log.debug("Obj: {}", object);
-    //        // reset for next test
-    //        resetOutput();
+    // buf.flip();
+    // // skip the first byte
+    // assertEquals(0, buf.get());
+    // // validate data
+    // assertEquals(DataTypes.CORE_STRING, in.readDataType());
+    // str = in.readString();
+    // assertEquals("onCuePoint", str);
+    // //Object object = Deserializer.deserialize(in, String.class);
+    // log.debug("onCuePoint: {}", str);
+    // //assertEquals(DataTypes.CORE_OBJECT, in.readDataType());
+    // object = Deserializer.deserialize(in, MetaCue.class);
+    // log.debug("Obj: {}", object);
+    // // reset for next test
+    // resetOutput();
   }
 
   @Test
@@ -191,24 +192,24 @@ public class AMF3IOTest extends AbstractIOTest {
     ByteArray baIn = new ByteArray();
     baIn.writeBytes(
         new byte[] {
-          (byte) 0,
-          (byte) 0x1,
-          (byte) 0x42,
-          (byte) 0x1,
-          (byte) 0x42,
-          (byte) 0x1,
-          (byte) 0x42,
-          (byte) 0x1,
-          (byte) 0x42,
-          (byte) 0x1,
-          (byte) 0x42,
-          (byte) 0x1,
-          (byte) 0x42,
-          (byte) 0x1,
-          (byte) 0x42,
-          (byte) 0x1,
-          (byte) 0x42,
-          (byte) 0x99
+            (byte) 0,
+            (byte) 0x1,
+            (byte) 0x42,
+            (byte) 0x1,
+            (byte) 0x42,
+            (byte) 0x1,
+            (byte) 0x42,
+            (byte) 0x1,
+            (byte) 0x42,
+            (byte) 0x1,
+            (byte) 0x42,
+            (byte) 0x1,
+            (byte) 0x42,
+            (byte) 0x1,
+            (byte) 0x42,
+            (byte) 0x1,
+            (byte) 0x42,
+            (byte) 0x99
         });
     Serializer.serialize(out, baIn);
     dumpOutput();
@@ -241,28 +242,27 @@ public class AMF3IOTest extends AbstractIOTest {
   public void testVectorIntInput() {
     log.debug("\n Testing Vector<int>");
     // 0D090000000002000007D07FFFFFFF80000000
-    byte[] v =
-        new byte[] {
-          (byte) 0x0D,
-          (byte) 0x09,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x02,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x07,
-          (byte) 0xD0,
-          (byte) 0x7F,
-          (byte) 0xFF,
-          (byte) 0xFF,
-          (byte) 0xFF,
-          (byte) 0x80,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00
-        };
+    byte[] v = new byte[] {
+        (byte) 0x0D,
+        (byte) 0x09,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x02,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x07,
+        (byte) 0xD0,
+        (byte) 0x7F,
+        (byte) 0xFF,
+        (byte) 0xFF,
+        (byte) 0xFF,
+        (byte) 0x80,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00
+    };
 
     in = new Input(IoBuffer.wrap(v));
     List<Object> vectorOut = Deserializer.deserialize(in, null);
@@ -279,28 +279,27 @@ public class AMF3IOTest extends AbstractIOTest {
   public void testVectorUIntInput() {
     log.debug("\n Testing Vector<uint>");
     // 0E090000000002000007D0FFFFFFFF00000000
-    byte[] v =
-        new byte[] {
-          (byte) 0x0E,
-          (byte) 0x09,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x02,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x07,
-          (byte) 0xD0,
-          (byte) 0xFF,
-          (byte) 0xFF,
-          (byte) 0xFF,
-          (byte) 0xFF,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00
-        };
+    byte[] v = new byte[] {
+        (byte) 0x0E,
+        (byte) 0x09,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x02,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x07,
+        (byte) 0xD0,
+        (byte) 0xFF,
+        (byte) 0xFF,
+        (byte) 0xFF,
+        (byte) 0xFF,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00
+    };
     in = new Input(IoBuffer.wrap(v));
     List<Object> vectorOut = Deserializer.deserialize(in, null);
     // [2, 2000, 4294967295, 0]
@@ -316,68 +315,67 @@ public class AMF3IOTest extends AbstractIOTest {
   public void testVectorNumberInput() {
     log.debug("\n Testing Vector<Number>");
     // 0F0F003FF199999999999ABFF199999999999A7FEFFFFFFFFFFFFF0000000000000001FFF8000000000000FFF00000000000007FF0000000000000
-    byte[] v =
-        new byte[] {
-          (byte) 0x0F,
-          (byte) 0x0F,
-          (byte) 0x00,
-          (byte) 0x3F,
-          (byte) 0xF1,
-          (byte) 0x99,
-          (byte) 0x99,
-          (byte) 0x99,
-          (byte) 0x99,
-          (byte) 0x99,
-          (byte) 0x9A,
-          (byte) 0xBF,
-          (byte) 0xF1,
-          (byte) 0x99,
-          (byte) 0x99,
-          (byte) 0x99,
-          (byte) 0x99,
-          (byte) 0x99,
-          (byte) 0x9A,
-          (byte) 0x7F,
-          (byte) 0xEF,
-          (byte) 0xFF,
-          (byte) 0xFF,
-          (byte) 0xFF,
-          (byte) 0xFF,
-          (byte) 0xFF,
-          (byte) 0xFF,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x01,
-          (byte) 0xFF,
-          (byte) 0xF8,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0xFF,
-          (byte) 0xF0,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x7F,
-          (byte) 0xF0,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00
-        };
+    byte[] v = new byte[] {
+        (byte) 0x0F,
+        (byte) 0x0F,
+        (byte) 0x00,
+        (byte) 0x3F,
+        (byte) 0xF1,
+        (byte) 0x99,
+        (byte) 0x99,
+        (byte) 0x99,
+        (byte) 0x99,
+        (byte) 0x99,
+        (byte) 0x9A,
+        (byte) 0xBF,
+        (byte) 0xF1,
+        (byte) 0x99,
+        (byte) 0x99,
+        (byte) 0x99,
+        (byte) 0x99,
+        (byte) 0x99,
+        (byte) 0x9A,
+        (byte) 0x7F,
+        (byte) 0xEF,
+        (byte) 0xFF,
+        (byte) 0xFF,
+        (byte) 0xFF,
+        (byte) 0xFF,
+        (byte) 0xFF,
+        (byte) 0xFF,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x01,
+        (byte) 0xFF,
+        (byte) 0xF8,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0xFF,
+        (byte) 0xF0,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x7F,
+        (byte) 0xF0,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00
+    };
 
     in = new Input(IoBuffer.wrap(v));
     List<Double> vectorOut = Deserializer.deserialize(in, null);
@@ -395,43 +393,42 @@ public class AMF3IOTest extends AbstractIOTest {
     log.debug("\n Testing Vector<Object>");
     // 100700010607666f6f010a13256f72672e726564352e74673742e466f6f33000403
     // [foo, null, org.red5.test.Foo3[foo=0]] // Foo3 is a class instance
-    byte[] v2 =
-        new byte[] {
-          (byte) 0x10,
-          (byte) 0x07,
-          (byte) 0x00,
-          (byte) 0x01,
-          (byte) 0x06,
-          (byte) 0x07,
-          (byte) 0x66,
-          (byte) 0x6f,
-          (byte) 0x6f,
-          (byte) 0x01,
-          (byte) 0x0a,
-          (byte) 0x13,
-          (byte) 0x25,
-          (byte) 0x6f,
-          (byte) 0x72,
-          (byte) 0x67,
-          (byte) 0x2e,
-          (byte) 0x72,
-          (byte) 0x65,
-          (byte) 0x64,
-          (byte) 0x35,
-          (byte) 0x2e,
-          (byte) 0x74,
-          (byte) 0x65,
-          (byte) 0x73,
-          (byte) 0x74,
-          (byte) 0x2e,
-          (byte) 0x46,
-          (byte) 0x6f,
-          (byte) 0x6f,
-          (byte) 0x33,
-          (byte) 0x00,
-          (byte) 0x04,
-          (byte) 0x03
-        };
+    byte[] v2 = new byte[] {
+        (byte) 0x10,
+        (byte) 0x07,
+        (byte) 0x00,
+        (byte) 0x01,
+        (byte) 0x06,
+        (byte) 0x07,
+        (byte) 0x66,
+        (byte) 0x6f,
+        (byte) 0x6f,
+        (byte) 0x01,
+        (byte) 0x0a,
+        (byte) 0x13,
+        (byte) 0x25,
+        (byte) 0x6f,
+        (byte) 0x72,
+        (byte) 0x67,
+        (byte) 0x2e,
+        (byte) 0x72,
+        (byte) 0x65,
+        (byte) 0x64,
+        (byte) 0x35,
+        (byte) 0x2e,
+        (byte) 0x74,
+        (byte) 0x65,
+        (byte) 0x73,
+        (byte) 0x74,
+        (byte) 0x2e,
+        (byte) 0x46,
+        (byte) 0x6f,
+        (byte) 0x6f,
+        (byte) 0x33,
+        (byte) 0x00,
+        (byte) 0x04,
+        (byte) 0x03
+    };
 
     in = new Input(IoBuffer.wrap(v2));
     List<Object> vectorOut = Deserializer.deserialize(in, null);
@@ -448,137 +445,131 @@ public class AMF3IOTest extends AbstractIOTest {
   public void testVectorStringInput() {
     log.debug("\n Testing Vector<String>");
     // [Paul, ]
-    byte[] v =
-        new byte[] {
-          (byte) 0x10,
-          (byte) 0x05,
-          (byte) 0x00,
-          (byte) 0x01,
-          (byte) 0x06,
-          (byte) 0x09,
-          (byte) 0x50,
-          (byte) 0x61,
-          (byte) 0x75,
-          (byte) 0x6c,
-          (byte) 0x06,
-          (byte) 0x01
-        };
+    byte[] v = new byte[] {
+        (byte) 0x10,
+        (byte) 0x05,
+        (byte) 0x00,
+        (byte) 0x01,
+        (byte) 0x06,
+        (byte) 0x09,
+        (byte) 0x50,
+        (byte) 0x61,
+        (byte) 0x75,
+        (byte) 0x6c,
+        (byte) 0x06,
+        (byte) 0x01
+    };
     // [Paul, Paul]
-    byte[] v1 =
-        new byte[] {
-          (byte) 0x10,
-          (byte) 0x05,
-          (byte) 0x00,
-          (byte) 0x01,
-          (byte) 0x06,
-          (byte) 0x09,
-          (byte) 0x50,
-          (byte) 0x61,
-          (byte) 0x75,
-          (byte) 0x6c,
-          (byte) 0x06,
-          (byte) 0x00
-        };
+    byte[] v1 = new byte[] {
+        (byte) 0x10,
+        (byte) 0x05,
+        (byte) 0x00,
+        (byte) 0x01,
+        (byte) 0x06,
+        (byte) 0x09,
+        (byte) 0x50,
+        (byte) 0x61,
+        (byte) 0x75,
+        (byte) 0x6c,
+        (byte) 0x06,
+        (byte) 0x00
+    };
     // [Paul, Paul, Paul]
-    byte[] v2 =
-        new byte[] {
-          (byte) 0x10,
-          (byte) 0x07,
-          (byte) 0x00,
-          (byte) 0x01,
-          (byte) 0x06,
-          (byte) 0x09,
-          (byte) 0x50,
-          (byte) 0x61,
-          (byte) 0x75,
-          (byte) 0x6c,
-          (byte) 0x06,
-          (byte) 0x00,
-          (byte) 0x06,
-          (byte) 0x00
-        };
+    byte[] v2 = new byte[] {
+        (byte) 0x10,
+        (byte) 0x07,
+        (byte) 0x00,
+        (byte) 0x01,
+        (byte) 0x06,
+        (byte) 0x09,
+        (byte) 0x50,
+        (byte) 0x61,
+        (byte) 0x75,
+        (byte) 0x6c,
+        (byte) 0x06,
+        (byte) 0x00,
+        (byte) 0x06,
+        (byte) 0x00
+    };
     // [Paul, Tawnya]
-    byte[] v3 =
-        new byte[] {
-          (byte) 0x10,
-          (byte) 0x05,
-          (byte) 0x00,
-          (byte) 0x01,
-          (byte) 0x06,
-          (byte) 0x09,
-          (byte) 0x50,
-          (byte) 0x61,
-          (byte) 0x75,
-          (byte) 0x6c,
-          (byte) 0x06,
-          (byte) 0x0d,
-          (byte) 0x54,
-          (byte) 0x61,
-          (byte) 0x77,
-          (byte) 0x6e,
-          (byte) 0x79,
-          (byte) 0x61
-        };
+    byte[] v3 = new byte[] {
+        (byte) 0x10,
+        (byte) 0x05,
+        (byte) 0x00,
+        (byte) 0x01,
+        (byte) 0x06,
+        (byte) 0x09,
+        (byte) 0x50,
+        (byte) 0x61,
+        (byte) 0x75,
+        (byte) 0x6c,
+        (byte) 0x06,
+        (byte) 0x0d,
+        (byte) 0x54,
+        (byte) 0x61,
+        (byte) 0x77,
+        (byte) 0x6e,
+        (byte) 0x79,
+        (byte) 0x61
+    };
 
     // [1.0, 3.0, aaa, 5.0, aaa, aaa, 5.0, bb, bb]
-    byte[] v4 =
-        new byte[] {
-          (byte) 0x10,
-          (byte) 0x13,
-          (byte) 0x00,
-          (byte) 0x01,
-          (byte) 0x04,
-          (byte) 0x01,
-          (byte) 0x04,
-          (byte) 0x03,
-          (byte) 0x06,
-          (byte) 0x07,
-          (byte) 0x61,
-          (byte) 0x61,
-          (byte) 0x61,
-          (byte) 0x04,
-          (byte) 0x05,
-          (byte) 0x06,
-          (byte) 0x00,
-          (byte) 0x06,
-          (byte) 0x00,
-          (byte) 0x04,
-          (byte) 0x05,
-          (byte) 0x06,
-          (byte) 0x05,
-          (byte) 0x62,
-          (byte) 0x62,
-          (byte) 0x06,
-          (byte) 0x02
-        };
+    byte[] v4 = new byte[] {
+        (byte) 0x10,
+        (byte) 0x13,
+        (byte) 0x00,
+        (byte) 0x01,
+        (byte) 0x04,
+        (byte) 0x01,
+        (byte) 0x04,
+        (byte) 0x03,
+        (byte) 0x06,
+        (byte) 0x07,
+        (byte) 0x61,
+        (byte) 0x61,
+        (byte) 0x61,
+        (byte) 0x04,
+        (byte) 0x05,
+        (byte) 0x06,
+        (byte) 0x00,
+        (byte) 0x06,
+        (byte) 0x00,
+        (byte) 0x04,
+        (byte) 0x05,
+        (byte) 0x06,
+        (byte) 0x05,
+        (byte) 0x62,
+        (byte) 0x62,
+        (byte) 0x06,
+        (byte) 0x02
+    };
     // [1.0, 3.0, aaa, [1, 2]]
-    byte[] v5 =
-        new byte[] {
-          (byte) 0x10,
-          (byte) 0x09,
-          (byte) 0x00,
-          (byte) 0x01,
-          (byte) 0x04,
-          (byte) 0x01,
-          (byte) 0x04,
-          (byte) 0x03,
-          (byte) 0x06,
-          (byte) 0x07,
-          (byte) 0x61,
-          (byte) 0x61,
-          (byte) 0x61,
-          (byte) 0x0d,
-          (byte) 0x05,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x01,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x00,
-          (byte) 0x02
-        };
+    byte[] v5 = new byte[] {
+        (byte) 0x10,
+        (byte) 0x09,
+        (byte) 0x00,
+        (byte) 0x01,
+        (byte) 0x04,
+        (byte) 0x01,
+        (byte) 0x04,
+        (byte) 0x03,
+        (byte) 0x06,
+        (byte) 0x07,
+        (byte) 0x61,
+        (byte) 0x61,
+        (byte) 0x61,
+        (byte) 0x0d,
+        (byte) 0x05,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x01,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x00,
+        (byte) 0x02
+    };
 
     in = new Input(IoBuffer.wrap(v5));
     List<Object> vectorOut = Deserializer.deserialize(in, null);
